9d83c87a3754839a2b236a87b1b2f63357682ccc
rowan <r.jarvis1@newcastle.ac.uk>
2023-11-03 13:15:33 +0000

    Added seperate button for viewing cocktail info

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cocktail App',
+      title: 'Random Cocktail',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,34 +20,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Map<String, dynamic>? cocktail;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cocktail App'),
+        title: Text('Random Cocktail'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('Generate Random Cocktail'),
-          onPressed: () async {
-            final response = await http.get(Uri.parse('https://www.thecocktaildb.com/api/json/v1/1/random.php'));
-            final Map<String, dynamic> cocktail = jsonDecode(response.body)['drinks'][0];
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => DetailPage(cocktail: cocktail)),
-            );
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Generate'),
+              onPressed: () async {
+                final response = await http.get(Uri.parse('https://www.thecocktaildb.com/api/json/v1/1/random.php'));
+                final Map<String, dynamic> newCocktail = json.decode(response.body)['drinks'][0];
+                setState(() {
+                  cocktail = newCocktail;
+                });
+              },
+            ),
+            if (cocktail != null)
+              ElevatedButton(
+                child: Text('View Cocktail'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DetailsPage(cocktail: cocktail!)),
+                  );
+                },
+              ),
+          ],
         ),
       ),
     );
   }
 }
 
-class DetailPage extends StatelessWidget {
+class DetailsPage extends StatelessWidget {
   final Map<String, dynamic> cocktail;
 
-  DetailPage({Key? key, required this.cocktail}) : super(key: key);
+  DetailsPage({required this.cocktail, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
